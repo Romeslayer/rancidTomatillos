438e51bc894fb83670950f51194a3c18885c0821
Katie Ammon <katie.ammon10@gmail.com>
2022-03-25 15:50:42 -0600

    Add Main element

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Main.css'
+import Card from './Card'
 
 const Main = ({movies}) => {
+
+     const movieCards = movies.map(movie => {
+          return(
+               <Card 
+                 id={movie.id}
+                 title={movie.title}
+                 backdropPath={movie.backdrop_path}
+                 avgRating={movie.avarage_rating}
+                 releaseDate={movie.release_date}
+               />
+          )
+     })
      return(
-          <h3>Hello!</h3>
+          <section className='movies-section'>
+               {movieCards}
+          </section>
      )
 
 }
