88e565ac7e1e50ddabdc29970eda378491bd1424
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-26 14:19:28 -0600

    Create layout to render a single movie to the page

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './Movie.css';
-import data from '../../singlePageData.js';
 
 
-const Movie = () => {
+
+const Movie = ({movie}) => {
+  movie = movie.movie;
+  let genres = movie.genres.join(', ');
+  let formatter = new Intl.NumberFormat('en-US', {
+style: 'currency',
+currency: 'USD',
+})
+
+const timeConvert = (num) => {
+  var hours = Math.floor(num / 60);
+  var minutes = num % 60;
+  return `${hours}:${minutes}`;
+}
 
   return (
-    <h1>Hello!</h1>
+    <section id={movie.id}>
+      <img className="backdrop" src={movie.backdrop_path} alt={`${movie.title}`} />
+      <div>
+        <img className="poster" src={movie.poster_path} alt={`${movie.title} promotional poster`} />
+        <div className="movieInfo">
+          <h2>{movie.title}</h2>
+          <p className="tagline">{movie.tagline}</p>
+          {movie.genres ? <p>Genres: {genres}</p> : ""}
+          {movie.average_rating ? <p>Average Rating: {movie.average_rating}</p> : ""}
+          {movie.overview ? <p>Overview: {movie.overview}</p> : ""}
+          {movie.runtime ? <p>Runtime: {timeConvert(movie.runtime)}</p> : ""}
+          { movie.budget ? <p>Total budget: {formatter.format(movie.budget)}</p> : ""}
+          { movie.revenue ? <p>Total revenue: {formatter.format(movie.revenue)}</p> : ""}
+        </div>
+      </div>
+
+    </section>
   )
 }
 
