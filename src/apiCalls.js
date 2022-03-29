1b8ce5c391fcce564dcbdf86b15777ffefc2b1a3
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-29 13:54:03 -0600

    Fix error handling logic and create DisplayMessage component

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,11 +7,7 @@ const getData = (endpoint, movieID) => {
     url = `https://rancid-tomatillos.herokuapp.com/api/v2/s${endpoint}`
   }
    return fetch(url)
-    .then(response => response.json())
-    .then(result => {
-      return (movieID ? result['movie'] : result[endpoint] )
-    })
-    .catch(err => err)
+    .then(response => response.json());
 }
 
 
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import Header from '../Header/Header.js'
 import Main from '../Main/Main.js'
 import Movie from '../Movie/Movie.js'
+import DisplayMessage from '../DisplayMessage/DisplayMessage.js'
 import getData from '../../apiCalls.js'
+
 import './App.css'
 
 class App extends Component {
@@ -29,9 +31,12 @@ class App extends Component {
     displayMovie = (event) => {
       let target = event.target.closest('.card');
       if(target.id) {
-        getData(`movies`, target.id).then(result => {
-          this.setState({singleMovie: result})
-        })
+        getData(`movies`, target.id)
+          .then(result => result.movie)
+          .then(data => {
+            this.setState({singleMovie: data})
+          })
+          .catch(err => console.log(err))
 
       }
     }
@@ -50,9 +55,6 @@ class App extends Component {
       this.setState({message: message})
     }
 
-    componentDidCatch(error, info) {
-      this.setState({hasError: true});
-    }
 
      render() {
           return(
@@ -60,6 +62,7 @@ class App extends Component {
                <Header filter={this.filter} displayHome={this.state.singleMovie} hideMovie={this.hideMovie} error={this.state.hasError} />
                {this.state.displayed && !this.state.singleMovie && !this.state.hasError && <Main movies={this.state.displayed} displayMovie={this.displayMovie}/>}
                {this.state.singleMovie && <Movie movie={this.state.singleMovie} />}
+               {this.state.hasError && <DisplayMessage message={this.state.message} />}
                </main>
 
           )
@@ -69,11 +72,13 @@ class App extends Component {
        console.log('I am still running')
 
        getData('movies')
-       .then(data => {
-        this.setState({movies: data, displayed:data})
-       })
-     }
+        .then(result => result['movies'])
+        .then(data => {
+         this.setState({movies: data, displayed:data})
+        })
+        .catch(err => this.setState({hasError: true, message: err}))
 
+       }
 
 }
 
