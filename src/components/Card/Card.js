321eb3abb66b98145fa01e711f63caf42edcd1d4
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-31 17:54:13 -0600

    Refactor to sytle guide

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import './Card.css'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import './Card.css';
+import {Link} from 'react-router-dom';
 
 const Card = ({id, posterPath, title}) => {
- return(
-      <Link to={`/movies/${id}`} >
-        <div id={id}
-        className='card'>
-          <img src={posterPath}
+  return(
+    <Link to={`/movies/${id}`} >
+      <div id={id} className='card'>
+        <img src={posterPath}
           alt={title}>
-          </img>
-        </div>
-      </Link>
- )
-
+        </img>
+      </div>
+    </Link>
+  )
 }
 
-export default Card
+export default Card;
