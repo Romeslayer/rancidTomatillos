8fc385221221e4a508e61ed84638055e987f9b46
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-31 17:32:49 -0600

    Refactor conditional render statements outside of return

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,68 +9,71 @@ import {Route, Switch, Redirect} from 'react-router-dom'
 import './App.css'
 
 class App extends Component {
-     constructor() {
-          super();
-          this.state = {
-               movies: null,
-               displayed: null,
-               hasError: false,
-               message: null
-          }
-     }
-
-
-
-      filter = (searchedTitle) => {
-        const filteredMovies = this.state.movies.filter(movie => movie.title.toLowerCase().includes(searchedTitle.toLowerCase()))
-        this.setState({displayed: filteredMovies })
-
+  constructor() {
+    super();
+    this.state = {
+      movies: null,
+      displayed: null,
+      hasError: false,
+      message: null
     }
+  }
 
-    deleteMessage = (event) => {
-      event.preventDefault();
-      this.setState({message: null})
+  filter = (searchedTitle) => {
+    const filteredMovies = this.state.movies.filter(movie => movie.title.toLowerCase().includes(searchedTitle.toLowerCase()));
+    this.setState({
+      displayed: filteredMovies
+    });
+  }
+
+  deleteMessage = (event) => {
+    event.preventDefault();
+    this.setState({
+      message: null
+    })
+  }
+
+  changeMessage = (message) => {
+    this.setState({
+      message: message
+    })
+  }
+
+  render() {
+    const main = (
+      <main className='app'>
+        <Redirect from='/' to='/movies' />
+
+        <Switch>
+          <Route exact path='/movies' render={ ()=>  <Header filter={this.filter} /> } />
+          <Route render={() => <Header />}   />
+        </Switch>
+
+        <Switch>
+          <Route exact path='/movies' render={ () => <Main movies={this.state.displayed} /> } />
+          <Route exact path='/movies/:id' render={({match}) => {
+            return <Movie id={match.params.id}/>
+          }}/>
+          <Route path='/error' render={() => <DisplayMessage message={this.state.message} /> } />
+        </Switch>
+      </main>
+    )
+
+    return (
+      <React.Fragment>
+        {main}
+      </React.Fragment>
+    )
+  }
+
+  componentDidMount() {
+    getData('movies')
+     .then(result => result['movies'])
+     .then(data => {
+      this.setState({movies: data, displayed:data})
+     })
+     .catch(err => this.setState({hasError: true, message: err.message}))
     }
-
-    changeMessage = (message) => {
-      this.setState({message: message})
-    }
-
-     render() {
-          return(
-               <main className='app'>
-               <Redirect from='/' to='/movies' />
-               <Switch>
-                 <Route exact path='/movies' render={ ()=>  <Header filter={this.filter} /> } />
-                 <Route render={() => <Header />}   />
-               </Switch>
-               {this.state.singleMovie && <Movie movie={this.state.singleMovie} />}
-               {this.state.hasError && <DisplayMessage message={this.state.message} />}
-                <Switch>
-
-                  <Route exact path='/movies' render={ () =>  <Main movies={this.state.displayed} /> }
-                  />
-                  <Route exact path='/movies/:id' render={({match}) => {
-                    return <Movie id={match.params.id}/>
-                  }}
-                  />
-                  <Route path='/error' render={() => <DisplayMessage message={this.state.message} /> }
-                  />
-                </Switch>
-               </main>
-
-          )
-     }
-
-     componentDidMount() {
-       getData('movies')
-        .then(result => result['movies'])
-        .then(data => {
-         this.setState({movies: data, displayed:data})
-        })
-        .catch(err => this.setState({hasError: true, message: err.message}))
-       }
-
 }
 
 export default App;
